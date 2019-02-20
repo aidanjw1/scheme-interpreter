1b92dbb0efe237a75910bbaa9a7dac29a3f7e705
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-02-20 16:20:44 -0600

    Initial setup

---
+++ src/interpreter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include "value.h"
+#include "interpreter.h"
+#include "talloc.h"
+#include "assert.h"
+#include "linkedlist.h"
+#include "parser.h"
---
+++ src/linkedlist.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "linkedlist.h"
+#include "value.h"
+#include "talloc.h"
+#include "assert.h"
+
